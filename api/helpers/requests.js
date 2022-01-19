d126147723c2aef96a045492649ac257ba5d4380
Kelvin Stephano <kstephano@gmail.com>
2022-01-19 20:35:00 +0000

    getQuestions now returns the array of questions

--- api/helpers/requests.js
+++ api/helpers/requests.js
@@ -7,11 +7,13 @@ const axios = require('axios');
  * @param {Must be 'easy', 'medium', 'hard'} difficulty 
  * @returns The response containing questions
  */
-async function getQuestions(numOfQuestions, category, difficulty) {
+async function getQuestions(numOfQuestions, categoryId, difficulty) {
+    const difficultyLowerCase = difficulty.toLowerCase();
     try {
-        const response = await axios.get(`https://opentdb.com/api.php?amount=${numOfQuestions}&category=${category}&difficulty=${difficulty}&type=multiple`);
-        console.log(response);
-        return response;
+        const response = await axios.get(`https://opentdb.com/api.php?amount=${numOfQuestions}&category=${categoryId}&difficulty=${difficultyLowerCase}&type=multiple`);
+        const data = response.data.results;
+        console.log(data)
+        return data;
     } catch (error) {
         console.error(error);
     }
