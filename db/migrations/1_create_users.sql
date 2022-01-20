4a5308cb294cd5664b26f1e7a83392d6164a3adb
Kelvin Stephano <kstephano@gmail.com>
2022-01-20 13:23:34 +0000

    removed not null for score

--- db/migrations/1_create_users.sql
+++ db/migrations/1_create_users.sql
@@ -3,6 +3,6 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     id serial PRIMARY KEY,
     username varchar(1000) NOT NULL,
-    score decimal (5,2) NOT NULL,
+    score INT,
     lobby_id int NOT NULL
 );
